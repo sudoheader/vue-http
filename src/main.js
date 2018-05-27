21cb84f6e6e557767dc9b060a371014c9e040884
Raymond Nazaryan <rayn2427@gmail.com>
2018-05-26 23:56:06 -0700

    Configured vue-resource globally

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
+Vue.http.options.root = 'https://vuejs-http-69070.firebaseio.com/data.json';
+
 new Vue({
   el: '#app',
   render: h => h(App)
