601f6940a5907de1b46cc441a46f9e0e651df794
Raymond Nazaryan <rayn2427@gmail.com>
2018-05-27 21:24:07 -0700

    Added intercepting requests

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import VueResource from 'vue-resource'
 Vue.use(VueResource);
 
 Vue.http.options.root = 'https://vuejs-http-69070.firebaseio.com/data.json';
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if (request.method == 'POST') {
+    request.method = 'PUT';
+  }
+  next();
+});
 
 new Vue({
   el: '#app',
